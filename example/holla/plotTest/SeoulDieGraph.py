6d4a87985e6203356e0548b02a4f0d2f8b4eeb26
realwater87 <realwater87@192.168.25.31>
2017-06-04 15:05:44 +0900

    소스 수정

--- example/holla/plotTest/SeoulDieGraph.py
+++ example/holla/plotTest/SeoulDieGraph.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+# 서울시 월별 연간 사망자 수 집계 
 import numpy as np
 import matplotlib.pyplot as plt
 from operator import eq
@@ -5,7 +7,7 @@ import csv
 
 def analyzeDie():
     
-    # csv file Data save
+    # csv 파일 읽어오기
     pieces = []
     datafile = 'D:\\plotTest\\ym_die.csv'
     with open(datafile, 'rt') as f :
@@ -13,7 +15,7 @@ def analyzeDie():
         for d in data:
             pieces.append(d)
     
-    # data array process
+    # csv 파일 데이터 배열로 만들기
     bf_date = ''
     dieCol = 0
     dieRow = 0
@@ -22,21 +24,22 @@ def analyzeDie():
         if eq(bf_date, '') == True :
             bf_date = date[:4]
             
-        elif eq(bf_date, date[:4]) == False :
+        elif eq(bf_date, date[:4]) == False : # 연도별로 데이터 담기
             bf_date = date[:4]
             dieCol += 1
             dieRow = 0
         
+        # 행은 연도 열은 월기준으로 데이터를 만든다.
         dieArray[dieCol].insert(dieRow, dieCnt)   
 
     month = ['1','2','3','4','5','6','7','8','9','10','11','12']
     year  = ['2010', '2011', '2012', '2013', '2014', '2015', '2016']
     color = ['b','g','r','c','m','y','k']
     
-    n_groups = 12
+    n_groups = 12 # 노출되는 그래프 x축 개수
 
     index = np.arange(n_groups)
-    bar_width = 0.1
+    bar_width = 0.1 # 막대그래프 넓이
      
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
@@ -54,9 +57,9 @@ def analyzeDie():
         width_g = width_g + bar_width
         cnt = cnt + 1
 
-    plt.xlabel('Year')
-    plt.ylabel('Count')
-    plt.title('Analyze Die Graph')
+    plt.xlabel('Year') # X축 제목
+    plt.ylabel('Count') # Y축 제목
+    plt.title('Analyze Die Graph') # 메인 제목 설정
     plt.xticks(index + bar_width, month)
     plt.legend()
     plt.tight_layout()
